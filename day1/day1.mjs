49c257fa1757b17a50552b5be0a322d8828d60db
Joaquim Esteves <joaquimbve@hotmail.com>
2022-03-09 17:40:27 +0100

    🐛 Now uses actual input.txt

--- day1/day1.mjs
+++ day1/day1.mjs
@@ -1,3 +1,14 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+// on ESModule mode we don't have access to __dirname
+// So this weirdness is required
+// See: https://bobbyhadz.com/blog/javascript-dirname-is-not-defined-in-es-module-scope
+export const uses_input = (filename = fileURLToPath(import.meta.url)) =>
+  fs.readFileSync(path.join(path.dirname(filename), "input.txt"), "utf8");
+
+// eslint-disable-next-line no-unused-vars
 const test_input = `\
 199
 200
@@ -28,12 +39,15 @@ const part_1_one_line = (text) =>
   text
     .split("\n")
     .map(Number)
-    .reduce(({ counter, prev }, next) => ({
-      prev: next,
-      counter: prev === undefined ? 0 : next > prev ? counter + 1 : counter,
-      // Note passing an empty object.
-      // If we don't JS skips over the first iteration.
-    }), {}).counter;
+    .reduce(
+      ({ counter, prev }, next) => ({
+        prev: next,
+        counter: prev === undefined ? 0 : next > prev ? counter + 1 : counter,
+        // Note passing an empty object.
+        // If we don't JS skips over the first iteration.
+      }),
+      {}
+    ).counter;
 
 function part_2(text) {
   let [a, b, c, ...input] = text.split("\n").map(Number);
@@ -48,9 +62,10 @@ function part_2(text) {
   ).counter;
 }
 
+const text_input = uses_input();
 console.log("-- Part 1 --");
-console.log(part_1(test_input));
+console.log(part_1(text_input));
 console.log("(one line)");
-console.log(part_1_one_line(test_input));
+console.log(part_1_one_line(text_input));
 console.log("-- Part 2 --");
-console.log(part_2(test_input));
+console.log(part_2(text_input));
